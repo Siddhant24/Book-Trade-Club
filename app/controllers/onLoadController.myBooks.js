fa1bab33ffd68432a0042f27e2b6a838bfc7a629
Siddhant Bhakar <sidbhakar@gmail.com>
2017-07-16 11:30:06 +0000

    added request text display and click control functions

--- app/controllers/onLoadController.myBooks.js
+++ app/controllers/onLoadController.myBooks.js
@@ -1,11 +1,35 @@
 'use strict';
 
+var requestText = document.querySelector('.request-text');
+var bookList = document.querySelector('.book-list');
+
 function getMyBooks(){
     return new Promise(function(resolve, reject){
-        console.log("hi");
         ajaxFunctions.ajaxRequest('GET', appUrl + '/myBooks', function(data){
+            console.log("data received");
             resolve(JSON.parse(data));
         });
+    });
+}
+
+function removeBook(e){
+    ajaxFunctions.ajaxPostRequest({book_id: e.id}, appUrl + '/myBooks', function(msg){
+        console.log(msg);
+        window.location.reload(true);
+    });
+}
+
+function approveRequest(e){
+    ajaxFunctions.ajaxPostRequest({book_id: e.id.slice(1)}, appUrl + '/request', function(msg){
+        console.log(msg);
+        window.location.reload(true);
+    });
+}
+
+function cancelRequest(e){
+    ajaxFunctions.ajaxPostRequest({book_id: e.id.slice(1)}, appUrl + '/trade', function(msg){
+        console.log(msg);
+        window.location.reload(true);
     });
 }
 
@@ -13,14 +37,49 @@ function getMyBooks(){
 (function(){
     
     ajaxFunctions.ready(function(){
-        
-        document.getElementById('my-requests').addEventListener('click', function(){
-            getMyBooks().then(function(data){
-                console.log("hi");
-               console.log(data); 
+        getMyBooks().then(function(data){
+            console.log(data); 
+            return data;
+        }).then(function(data){
+            
+            data.books.forEach(function(book){
+                if(book.status.code !== 'traded'){
+                    var div = document.createElement('div');
+                    div.setAttribute('class', 'book-item');
+                    div.innerHTML = '<button class="btn-link"><i class="fa fa-times remove" aria-hidden="true" onclick="removeBook(this)" style="font-size:25px;" id=\"' + book._id + '\"></i></button><img src=\"' + book.cover + '\">';
+                    bookList.append(div);
+                }
+            });
+            
+            document.getElementById('my-requests').addEventListener('click', function(){
+                requestText.innerHTML = '';
+                data.myRequests.forEach(function(request){
+                    requestText.innerHTML += '<div class="text-primary">Title:&nbsp;' + request.title + '<button class="btn-link"><i class="fa fa-times cancel-request" aria-hidden="true" onclick="cancelRequest(this)" style="font-size:25px;" id=\".' + request._id + '\"></i></button></div>';
+                });
+            });
+            
+            document.getElementById('requests-for-me').addEventListener('click', function(){
+                requestText.innerHTML = '';
+                data.books.forEach(function(book){
+                    if(book.status.code === 'requested'){
+                        requestText.innerHTML = '<div class="text-center text-danger">Title:&nbsp;' + book.title + '<button class="btn-link"><i class="fa fa-check approve-request" aria-hidden="true" onclick="approveRequest(this)" style="font-size:25px;" id=\"*' + book._id + '\"></i></button><button class="btn-link"><i class="fa fa-times cancel-request" aria-hidden="true" onclick="cancelRequest(this)" style="font-size:25px;" id=\"&' + book._id + '\"></i></button></div>';
+                    }
+                });
+            });
+            
+            document.getElementById('traded').addEventListener('click', function(){
+               requestText.innerHTML = '';
+                data.books.forEach(function(book){
+                    if(book.status.code === 'traded'){
+                        requestText.innerHTML = '<div class="text-right text-success"><span class="trade-success">Title:&nbsp;' + book.title + '</span></div>';
+                    }
+                });
             });
         });
-       
+        
+        document.querySelector('.home').addEventListener('click', function(){
+            window.location.href = appUrl;
+        });
     });
     
 })();
